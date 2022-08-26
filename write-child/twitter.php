62b994f9a7d962cb8e95c98436768376337efd43
EC2 Default User <ec2-user@ip-172-31-83-222.ec2.internal>
2022-08-26 14:21:58 +0000

    add child theme

---
+++ write-child/twitter.php
@@ -0,0 +1,7 @@
+<meta name="twitter:card" content="summary_large_image" /> 
+<meta name="twitter:site" content="@kugyu10" /> 
+<meta property="og:url" content="<?php echo get_permalink(); ?>" />
+<meta property="og:title" content="オタクがうつを治すブログ" /> 
+<meta property="og:description" content="オタクがうつを治すブログ" /> 
+<meta property="og:image" content="<"https://otaku-utsu.com/wp-content/uploads/2022/08/cropped-utsu-header.webp" />
+
